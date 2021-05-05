9e6cda9f536ab7d3a46ca9219a46d4a3a5638d35
AndreLuvetama <mbiyavanga.luvetama@oi.net.br>
2021-05-05 19:39:46 -0300

    start to implement board Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Board {
+	private int rows; //qtde de linhas
+	private int colunms;
+	private Piece[][] pieces;
+	public Board(int rows, int colunms) {
+		this.rows = rows;
+		this.colunms = colunms;
+		pieces = new Piece[rows][colunms];
+	}
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getColunms() {
+		return colunms;
+	}
+	public void setColunms(int colunms) {
+		this.colunms = colunms;
+	}
+	public Piece[][] getPieces() {
+		return pieces;
+	}
+	public void setPieces(Piece[][] pieces) {
+		this.pieces = pieces;
+	}
+	
+	
+	
+	
+
+}
