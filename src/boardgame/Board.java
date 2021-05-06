2348959199a90e5b747b11e621ec8693e631784b
AndreLuvetama <mbiyavanga.luvetama@oi.net.br>
2021-05-05 21:52:49 -0300

    Chess layer and printing board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,8 +28,12 @@ public class Board {
 		this.pieces = pieces;
 	}
 	
+	public Piece piece(int row, int colunm) {
+		return pieces[row] [colunm];
+	}
 	
-	
-	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 
 }
